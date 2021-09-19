cfd05528ee81d6f972036c28c0e61c1e12667e88
cosby1992 <andersbirkedahl@hotmail.com>
2021-09-19 17:26:40 +0200

    Test feature implemented for celsius to fahrenheit conversion.

--- src/test/java/dk/cosby/cph/test/cucumber/TemperatureConverterStepDefinitions.java
+++ src/test/java/dk/cosby/cph/test/cucumber/TemperatureConverterStepDefinitions.java
@@ -1,6 +1,5 @@
 package dk.cosby.cph.test.cucumber;
 
-import dk.cosby.cph.test.TemperatureConverter;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,24 +12,28 @@ import static org.junit.Assert.assertEquals;
  */
 public class TemperatureConverterStepDefinitions {
 
-    private double fahrenheit;
+    private double temp;
     private double actual;
     private double delta = 0.001; // The precision in the test should be precise to two decimals
 
     @Given("a {double}")
-    public void aFahrenheitTemp(double fahrenheit) {
-        this.fahrenheit = fahrenheit;
+    public void aTemp(double temp) {
+        this.temp = temp;
     }
 
-    @When("Converting to celsius")
+    @When("Converting to Celsius")
     public void convertingToCelsius() {
         // Convert the temperature using the TemperatureConverter class
-        actual = dk.cosby.cph.test.TemperatureConverter.fahrenheitToCelsius(fahrenheit);
+        actual = dk.cosby.cph.test.TemperatureConverter.fahrenheitToCelsius(temp);
     }
 
     @Then("the result should be {double}")
-    public void theResultShouldBeCelsiusTemp(double expected) {
+    public void theResultShouldBeTheConvertedTemp(double expected) {
         // Test the result
         assertEquals(expected, actual, delta);
+    }
+
+    @When("Converting to Fahrenheit")
+    public void convertingToFahrenheit() {
     }
 }
--- src/test/resources/features/TemperatureConverter.feature
+++ src/test/resources/features/TemperatureConverter.feature
@@ -1,12 +1,33 @@
 
   Feature: Temperature Converter
 
-    The formula for converting fahrenheit to celsius is (°F − 32) × 5/9 = °C
+    The formula for converting Fahrenheit to Celsius is (°F − 32) × 5/9 = °C
 
-    Scenario Outline: Convert fahrenheit to Celsius
+    Scenario Outline: Convert Fahrenheit to Celsius
       Given a <fahrenheit temp>
-      When Converting to celsius
+      When Converting to Celsius
       Then the result should be <celsius temp>
+      Examples:
+        | fahrenheit temp | celsius temp |
+        | -459.67         | -273.15      |
+        | -30.0           | -34.44       |
+        | -10.0           | -23.33       |
+        | 0.0             | -17.78       |
+        | 10.0            | -12.22       |
+        | 32.0            | 0.0          |
+        | 40.0            | 4.44         |
+        | 50.0            | 10.0         |
+        | 60.0            | 15.56        |
+        | 70.0            | 21.11        |
+        | 500.0           | 260.0        |
+        | 1000.0          | 537.78       |
+        | 1000000.0       | 555537.78    |
+        | 10000000.0      | 5555537.78   |
+
+    Scenario Outline: Convert Celsius to Fahrenheit
+      Given a <celsius temp>
+      When Converting to Fahrenheit
+      Then the result should be <fahrenheit temp>
       Examples:
         | fahrenheit temp | celsius temp |
         | -459.67         | -273.15      |
