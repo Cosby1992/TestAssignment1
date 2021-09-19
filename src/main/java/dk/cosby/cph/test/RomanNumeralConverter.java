d513e369984f1d4641cd12a843244334f4d61d63
stabz <dmitrokaraush@gmail.com>
2021-09-19 23:06:29 +0200

    RomanNumeralConverter added cases to the feature, change in code

--- src/main/java/dk/cosby/cph/test/RomanNumeralConverter.java
+++ src/main/java/dk/cosby/cph/test/RomanNumeralConverter.java
@@ -4,18 +4,37 @@ public class RomanNumeralConverter {
 
         public static String convert(int arabicNumeral){
 
+                StringBuilder romanNumeralBuilder = new StringBuilder();
 
-                return switch (arabicNumeral) {
-                        case 1 -> "I";
-                        case 5 -> "V";
-                        case 10 -> "X";
-                        case 50 -> "L";
-                        case 100 -> "C";
-                        case 500 -> "D";
-                        case 1000 -> "M";
-                        default -> "default";
-                };
+                while(arabicNumeral >=  1000){
+                        romanNumeralBuilder.append('M');
+                        arabicNumeral -= 1000;
+                }
+                while(arabicNumeral >= 500){
+                        romanNumeralBuilder.append('D');
+                        arabicNumeral -= 500;
+                }
+                while(arabicNumeral >= 100){
+                        romanNumeralBuilder.append('C');
+                        arabicNumeral -= 100;
+                }
+                while(arabicNumeral >= 50){
+                        romanNumeralBuilder.append('L');
+                        arabicNumeral -= 50;
+                }
+                while(arabicNumeral >= 10){
+                        romanNumeralBuilder.append('X');
+                        arabicNumeral -= 10;
+                }
+                while(arabicNumeral >= 5){
+                        romanNumeralBuilder.append('V');
+                        arabicNumeral -= 5;
+                }
+                while(arabicNumeral >= 1){
+                        romanNumeralBuilder.append('I');
+                        arabicNumeral -= 1;
+                }
 
-
+                return romanNumeralBuilder.toString();
         }
 }
