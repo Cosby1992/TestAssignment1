2763f4a1f3add883ba8b02dc6e1280af1acc2f8f
stabz <dmitrokaraush@gmail.com>
2021-09-19 23:22:46 +0200

    26 testcases on RomanNumeralConverter

--- src/main/java/dk/cosby/cph/test/RomanNumeralConverter.java
+++ src/main/java/dk/cosby/cph/test/RomanNumeralConverter.java
@@ -7,31 +7,55 @@ public class RomanNumeralConverter {
                 StringBuilder romanNumeralBuilder = new StringBuilder();
 
                 while(arabicNumeral >=  1000){
-                        romanNumeralBuilder.append('M');
+                        romanNumeralBuilder.append("M");
                         arabicNumeral -= 1000;
                 }
+                while(arabicNumeral >=  900){
+                        romanNumeralBuilder.append("CM");
+                        arabicNumeral -= 900;
+                }
                 while(arabicNumeral >= 500){
-                        romanNumeralBuilder.append('D');
+                        romanNumeralBuilder.append("D");
                         arabicNumeral -= 500;
                 }
+                while(arabicNumeral >= 400){
+                        romanNumeralBuilder.append("CD");
+                        arabicNumeral -= 400;
+                }
                 while(arabicNumeral >= 100){
-                        romanNumeralBuilder.append('C');
+                        romanNumeralBuilder.append("C");
                         arabicNumeral -= 100;
                 }
+                while(arabicNumeral >= 90){
+                        romanNumeralBuilder.append("XC");
+                        arabicNumeral -= 90;
+                }
                 while(arabicNumeral >= 50){
-                        romanNumeralBuilder.append('L');
+                        romanNumeralBuilder.append("L");
                         arabicNumeral -= 50;
                 }
+                while(arabicNumeral >= 40){
+                        romanNumeralBuilder.append("XL");
+                        arabicNumeral -= 40;
+                }
                 while(arabicNumeral >= 10){
-                        romanNumeralBuilder.append('X');
+                        romanNumeralBuilder.append("X");
                         arabicNumeral -= 10;
                 }
+                while(arabicNumeral >= 9){
+                        romanNumeralBuilder.append("IX");
+                        arabicNumeral -= 9;
+                }
                 while(arabicNumeral >= 5){
-                        romanNumeralBuilder.append('V');
+                        romanNumeralBuilder.append("V");
                         arabicNumeral -= 5;
                 }
+                while(arabicNumeral >= 4){
+                        romanNumeralBuilder.append("IV");
+                        arabicNumeral -= 4;
+                }
                 while(arabicNumeral >= 1){
-                        romanNumeralBuilder.append('I');
+                        romanNumeralBuilder.append("I");
                         arabicNumeral -= 1;
                 }
 
--- src/test/resources/features/RomanNumeralConverter.feature
+++ src/test/resources/features/RomanNumeralConverter.feature
@@ -6,25 +6,32 @@
       When converting to roman numerals
       Then the result should be <roman numeral>
       Examples:
-        | integer | roman numeral |
-        | 1       | "I"           |
-        | 2       | "II"          |
-        | 3       | "III"         |
-        | 4       | "IV"          |
-        | 5       | "V"           |
-        | 6       | "VI"          |
-        | 7       | "VII"         |
-        | 8       | "VIII"        |
-        | 9       | "IX"          |
-        | 10      | "X"           |
-        | 19      | "IX"          |
-        | 20      | "XX"          |
-        | 25      | "XV"          |
-        | 50      | "L"           |
-        | 100     | "C"           |
-        | 134     | "CXXXIV"      |
-        | 500     | "D"           |
-        | 800     | "DCCC"        |
-        | 1000    | "M"           |
-        | 2000    | "MM"          |
+        | integer | roman numeral  |
+        | 1       | "I"            |
+        | 2       | "II"           |
+        | 3       | "III"          |
+        | 4       | "IV"           |
+        | 5       | "V"            |
+        | 6       | "VI"           |
+        | 7       | "VII"          |
+        | 8       | "VIII"         |
+        | 9       | "IX"           |
+        | 10      | "X"            |
+        | 19      | "XIX"          |
+        | 20      | "XX"           |
+        | 25      | "XXV"          |
+        | 50      | "L"            |
+        | 100     | "C"            |
+        | 134     | "CXXXIV"       |
+        | 419     | "CDXIX"        |
+        | 500     | "D"            |
+        | 558     | "DLVIII"       |
+        | 800     | "DCCC"         |
+        | 832     | "DCCCXXXII"    |
+        | 888     | "DCCCLXXXVIII" |
+        | 923     | "CMXXIII"      |
+        | 986     | "CMLXXXVI"     |
+        | 999     | "CMXCIX"       |
+        | 1000    | "M"            |
+        | 2000    | "MM"           |
 
