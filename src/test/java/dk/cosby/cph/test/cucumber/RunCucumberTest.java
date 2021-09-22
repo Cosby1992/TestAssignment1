95ca6cdd8f5a742a06998dde67a6d7c91ce84a81
cosby1992 <andersbirkedahl@hotmail.com>
2021-09-22 12:36:07 +0200

    Cucumber runner now test all features in each run

--- src/test/java/dk/cosby/cph/test/cucumber/RunCucumberTest.java
+++ src/test/java/dk/cosby/cph/test/cucumber/RunCucumberTest.java
@@ -7,10 +7,10 @@ import org.junit.runner.RunWith;
 /**
  * Cucumber runner class, handles options and running of the tests
  */
-// "src/test/resources/features/TemperatureConverter.feature",
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/RomanNumeralConverter.feature"},
+        features = {"src/test/resources/features/RomanNumeralConverter.feature",
+                "src/test/resources/features/TemperatureConverter.feature"},
         glue = {"dk/cosby/cph/test/cucumber"})
 public class RunCucumberTest {
 }
