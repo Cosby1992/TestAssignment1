405bd953eab0d1ddf92ef98072b2bbbfb877d157
cosby1992 <andersbirkedahl@hotmail.com>
2021-09-22 11:30:31 +0200

    Refactor and comments

--- src/main/java/dk/cosby/cph/test/RomanNumeralConverter.java
+++ src/main/java/dk/cosby/cph/test/RomanNumeralConverter.java
@@ -1,65 +1,77 @@
 package dk.cosby.cph.test;
 
+/**
+ * This class handles conversion from arabic numerals to roman numerals
+ *
+ * @author Anders, Dmitro, Ditlev
+ */
 public class RomanNumeralConverter {
 
+        /**
+         * Converts an integer to a roman numeral ie. input 9 returns "IX"
+         * @param arabicNumeral the number to convert
+         * @return the number converted to a roman numeral
+         */
         public static String convert(int arabicNumeral){
 
                 // Guard expression for negative numbers
                 if(arabicNumeral < 0) return "Roman numerals does not support negative numbers";
 
+                // StringBuilder to build and contain the result (Eg. "XIV")
                 StringBuilder romanNumeralBuilder = new StringBuilder();
 
-                while(arabicNumeral >=  1000){
+                // Checking all cases
+                while(arabicNumeral >=  1000){ // counting M's (number of thousands)
                         romanNumeralBuilder.append("M");
                         arabicNumeral -= 1000;
                 }
-                while(arabicNumeral >=  900){
+                if(arabicNumeral >=  900){ // counting CM's (number of nine hundreds)
                         romanNumeralBuilder.append("CM");
                         arabicNumeral -= 900;
                 }
-                while(arabicNumeral >= 500){
+                if(arabicNumeral >= 500){ // counting D's (number of five hundreds)
                         romanNumeralBuilder.append("D");
                         arabicNumeral -= 500;
                 }
-                while(arabicNumeral >= 400){
+                if(arabicNumeral >= 400){ // counting CD's (number of four hundreds)
                         romanNumeralBuilder.append("CD");
                         arabicNumeral -= 400;
                 }
-                while(arabicNumeral >= 100){
+                while(arabicNumeral >= 100){ // counting C's (number of hundreds)
                         romanNumeralBuilder.append("C");
                         arabicNumeral -= 100;
                 }
-                while(arabicNumeral >= 90){
+                if(arabicNumeral >= 90){ // counting XC's (number of nineties)
                         romanNumeralBuilder.append("XC");
                         arabicNumeral -= 90;
                 }
-                while(arabicNumeral >= 50){
+                if(arabicNumeral >= 50){ // counting L's (number of fifties)
                         romanNumeralBuilder.append("L");
                         arabicNumeral -= 50;
                 }
-                while(arabicNumeral >= 40){
+                if(arabicNumeral >= 40){ // counting XL's (number of forties)
                         romanNumeralBuilder.append("XL");
                         arabicNumeral -= 40;
                 }
-                while(arabicNumeral >= 10){
+                while(arabicNumeral >= 10){ // counting X's (number of tens)
                         romanNumeralBuilder.append("X");
                         arabicNumeral -= 10;
                 }
-                while(arabicNumeral >= 9){
+                if(arabicNumeral >= 9){ // counting IX's (number of nines)
                         romanNumeralBuilder.append("IX");
                         arabicNumeral -= 9;
                 }
-                while(arabicNumeral >= 5){
+                if(arabicNumeral >= 5){ // counting V's (number of fives)
                         romanNumeralBuilder.append("V");
                         arabicNumeral -= 5;
                 }
-                while(arabicNumeral >= 4){
+                if(arabicNumeral >= 4){ // counting IV's (number of fours)
                         romanNumeralBuilder.append("IV");
                         arabicNumeral -= 4;
                 }
-                while(arabicNumeral >= 1){
+                while(arabicNumeral >= 1){ // counting I's (number of ones)
                         romanNumeralBuilder.append("I");
-                        arabicNumeral -= 1;
+                        arabicNumeral--;
                 }
 
                 return romanNumeralBuilder.toString();
